21c0bac9bd4c24a3141e5f565a694c92dc786096
taochunyu <tao_chunyu@foxmail.com>
2020-06-09 00:37:39 +0800

    feat: ui web demo

---
+++ ui/examples/web/demo/greet.js
@@ -0,0 +1,3 @@
+export const greet = (instructions) => {
+    console.log(instructions);
+}
---
+++ ui/examples/web/src/lib.rs
@@ -0,0 +1,45 @@
+use wasm_bindgen::prelude::*;
+use js_sys::*;
+use ui::ui::UI;
+use ui::element::rectangular::{RectangularProps, Rectangular};
+use std::rc::Rc;
+
+#[wasm_bindgen(module = "../demo/greet")]
+extern "C" {
+    fn greet(instructions: Array);
+}
+
+#[wasm_bindgen]
+pub struct InstructionStream {
+    ui: UI,
+}
+
+#[wasm_bindgen]
+impl InstructionStream {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> InstructionStream {
+        InstructionStream {
+            ui: UI::new().unwrap(),
+        }
+    }
+
+    fn t(&mut self) -> Result<Vec<String>, String>{
+        let rect_props_1 = RectangularProps { width: 200, height: 100 };
+        let rect_1 = self.ui.create_element::<Rectangular>(rect_props_1)?;
+
+        let rect_props_2 = RectangularProps { width: 200, height: 100 };
+        let rect_2 = self.ui.create_element::<Rectangular>(rect_props_2)?;
+
+        self.ui.root_element.borrow_mut().append_child(Rc::clone(&rect_1));
+
+        rect_1.borrow_mut().append_child(rect_2)?;
+
+        Ok(self.ui.flush())
+    }
+
+    pub fn trigger(&mut self) {
+        let ins = self.t().unwrap().iter().map(JsValue::from).collect();
+
+        greet(ins);
+    }
+}
